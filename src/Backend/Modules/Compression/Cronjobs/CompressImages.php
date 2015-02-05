096a4505b012c673e98f45e0527a24a600a2f3a0
Jesse Dobbelaere <jesse@dobbelaere-ae.be>
2015-02-05 15:58:01 +0100

    Calculate percentage gain and cleanup code

--- src/Backend/Modules/Compression/Cronjobs/CompressImages.php
+++ src/Backend/Modules/Compression/Cronjobs/CompressImages.php
@@ -21,10 +21,18 @@ use Backend\Modules\Compression\Engine\Model as BackendCompressionModel;
  */
 class CompressImages extends BackendBaseCronjob
 {
+    /**
+     * API key needed by the API.
+     *
+     * @var string
+     */
     private $apiKey;
 
-    private $filesPath;
-
+    /**
+     * All of the images that are inside the selected folders
+     *
+     * @var array The array containing all the images
+     */
     private $images = array();
 
 
@@ -34,7 +42,6 @@ class CompressImages extends BackendBaseCronjob
     public function execute()
     {
         parent::execute();
-        $this->filesPath = FRONTEND_FILES_PATH;
 
         // Get data from db
         $this->getData();
@@ -43,7 +50,13 @@ class CompressImages extends BackendBaseCronjob
         $this->compressImages();
     }
 
-    private function getData() {
+    /**
+     * Get the api key and the array containing all the images to be compressed.
+     *
+     * @throws \SpoonException
+     */
+    private function getData()
+    {
         // Get api key
         $this->apiKey = BackendModel::getModuleSetting($this->getModule(), 'api_key', null);
 
@@ -55,13 +68,14 @@ class CompressImages extends BackendBaseCronjob
         $folders = BackendCompressionModel::getAllFolders();
         $finder = new Finder();
 
-        foreach($folders as $folder) {
+        // Select all the jpg & png images inside the folder and add them to the images array
+        foreach ($folders as $folder) {
             $iterator = $finder
                 ->files()
                 ->name('/\.(jpg|jpeg|png)$/i')
-                ->in($this->filesPath . $folder['path']);
+                ->in(FRONTEND_FILES_PATH . $folder['path']);
 
-            foreach($iterator as $file) {
+            foreach ($iterator as $file) {
                 $this->images[] = array(
                     'filename' => $file->getFilename(),
                     'full_path' => $file->getRealpath(),
@@ -69,25 +83,26 @@ class CompressImages extends BackendBaseCronjob
                 );
             }
         }
-
     }
 
-    private function compressImages() {
+    /**
+     * Compress the images from the selected folders. Send them to the TinyPNG api.
+     */
+    private function compressImages()
+    {
+        print('Compressing ' . count($this->images) . ' images...' . "<br />\r\n");
 
-        print("Compressing " . count($this->images) . " images..." . "<br />\r\n");
-
-        foreach($this->images as $image) {
-
+        foreach ($this->images as $image) {
             $request = curl_init();
             curl_setopt_array($request, array(
-                CURLOPT_URL => "https://api.tinypng.com/shrink",
-                CURLOPT_USERPWD => "api:" . $this->apiKey,
+                CURLOPT_URL => 'https://api.tinypng.com/shrink',
+                CURLOPT_USERPWD => 'api:' . $this->apiKey,
                 CURLOPT_POSTFIELDS => file_get_contents($image['full_path']),
                 CURLOPT_BINARYTRANSFER => true,
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_HEADER => true,
                 /* Uncomment below if you have trouble validating our SSL certificate.
-				   Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem */
+                   Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem */
                 // CURLOPT_CAINFO => __DIR__ . "/cacert.pem",
                 CURLOPT_SSL_VERIFYPEER => true
             ));
@@ -97,7 +112,7 @@ class CompressImages extends BackendBaseCronjob
                 /* Compression was successful, retrieve output from Location header. */
                 $headers = substr($response, 0, curl_getinfo($request, CURLINFO_HEADER_SIZE));
                 foreach (explode("\r\n", $headers) as $header) {
-                    if (substr($header, 0, 10) === "Location: ") {
+                    if (substr($header, 0, 10) === 'Location: ') {
                         $request = curl_init();
                         curl_setopt_array($request, array(
                             CURLOPT_URL => substr($header, 10),
@@ -113,14 +128,14 @@ class CompressImages extends BackendBaseCronjob
                 $imageInfo = new \SplFileInfo($image['full_path']);
                 $image['file_size_compressed'] = $imageInfo->getSize();
                 $image['saved_bytes'] = (int) $image['file_size_original'] - (int) $image['file_size_compressed'];
+                $image['saved_percentage'] = (int) ($image['saved_bytes'] / $image['file_size_original'] * 100);
 
-                print("Compression succesful for image " . $image['filename'] . ". Saved " . number_format($image['saved_bytes'] / 1024, 2) . ' KB' . " bytes" . "<br />\r\n");
+                print('Compression succesful for image ' . $image['filename'] . '. Saved ' . number_format($image['saved_bytes'] / 1024, 2) . ' KB' . ' bytes. (' . $image['saved_percentage'] . '%)' . "<br />\r\n");
             } else {
                 // Something went wrong
                 print(curl_error($request));
-                print("Compression failed for image " . $image . "<br />\r\n");
+                print('Compression failed for image ' . $image . "<br />\r\n");
             }
-
         }
     }
 }
